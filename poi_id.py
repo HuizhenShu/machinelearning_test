51353484117d405f93522eaac1b02517903e2afe
stacy.shu <shuhuizhen@china-norm.com>
2018-01-02 20:03:41 +0800

    first revise

--- poi_id.py
+++ poi_id.py
@@ -7,13 +7,14 @@ import matplotlib.pyplot
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 from sklearn.feature_selection import SelectPercentile, f_classif
+import pandas as pd
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
 features_list = ['poi','salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 
 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 
 'director_fees','to_messages', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 
-'shared_receipt_with_poi','ft_with_poi'] # You will need to use more features
+'shared_receipt_with_poi'] # You will need to use more features,'ft_with_poi'
 
 
 ### Load the dictionary containing the dataset
@@ -23,13 +24,22 @@ with open("final_project_dataset.pkl", "r") as data_file:
 ### Task 2: Remove outliers
 
 data_dict.pop('TOTAL', 0)#Remove outliers 
+data_dict.pop('THE TRAVEL AGENCY IN THE PARK', 0)
+data_dict.pop('LOCKHART EUGENE E', 0)
 
+data_frame = pd.DataFrame(data_dict)
 
-
+# print data_frame
+# print features_list[:-1]
+for key in features_list[:-1]:
+	checkdata = data_frame.ix[key]
+	#print checkdata
+	#print checkdata.value_counts()#[0]#.ix['NaN']
 
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 for key in data_dict:
+	#print data_dict[key].keys()
 	data_dict[key]['ft_with_poi'] = data_dict[key]['from_this_person_to_poi']+data_dict[key]['from_poi_to_this_person']
 	if data_dict[key]['ft_with_poi'] =='NaNNaN':
 		#print data_dict[key]['ft_with_poi']
@@ -38,16 +48,16 @@ for key in data_dict:
 	#print data_dict[key]['ft_with_poi']
 my_dataset = data_dict
 
-print my_dataset.values()[0].keys()
+#print my_dataset.values()[0].keys()
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
-salarys = []
-maxsalary = 0
-for i in range(len(features)):
-	if features[i][0]>maxsalary:
-		maxsalary =features[i][0]
-	salarys.append(features[i][0])
+# salarys = []
+# maxsalary = 0
+# for i in range(len(features)):
+# 	if features[i][0]>maxsalary:
+# 		maxsalary =features[i][0]
+# 	salarys.append(features[i][0])
 
 
 
@@ -57,9 +67,9 @@ for i in range(len(features)):
 # matplotlib.pyplot.ylabel("salary")
 # matplotlib.pyplot.show()
 ##find out who has the extremely high salary
-for i in range(len(data_dict)):
-	if data_dict[data_dict.keys()[i]]['salary']==maxsalary:
-		pass
+# for i in range(len(data_dict)):
+# 	if data_dict[data_dict.keys()[i]]['salary']==maxsalary:
+# 		pass
 		#print data_dict.keys()[i]
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -77,13 +87,26 @@ from sklearn.neighbors.nearest_centroid import NearestCentroid
 
 from sklearn.metrics import accuracy_score,recall_score
 from sklearn.cross_validation import KFold
-kf = KFold(len(features),5)
+from sklearn.cross_validation import StratifiedShuffleSplit
+# kf = KFold(len(features),5)
 #print features
-for train_indices,test_indices in kf:
-	features_train = [features[ii] for ii in train_indices] 
-	features_test= [features[ii] for ii in test_indices]  
-	labels_train= [labels[ii] for ii in train_indices]  
-	labels_test= [labels[ii] for ii in test_indices] 
+
+cv = StratifiedShuffleSplit(labels, 5, random_state = 42)
+true_negatives = 0
+false_negatives = 0
+true_positives = 0
+false_positives = 0
+for train_idx, test_idx in cv: 
+    features_train = []
+    features_test  = []
+    labels_train   = []
+    labels_test    = []
+    for ii in train_idx:
+        features_train.append( features[ii] )
+        labels_train.append( labels[ii] )
+    for jj in test_idx:
+        features_test.append( features[jj] )
+        labels_test.append( labels[jj] )
 	#clf = GaussianNB()
 	clf = NearestCentroid(shrink_threshold =1.5)
 	#clf = neural_network.MLPClassifier(activation='relu')
@@ -123,3 +146,4 @@ for train_indices,test_indices in kf:
 	### generates the necessary .pkl files for validating your results.
 
 	dump_classifier_and_data(clf, my_dataset, features_list)
+
